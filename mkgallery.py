ada7ee756a37dade5a3705cf12d54e5b7f377354
hluk <hluk@email.cz>
2010-06-23 15:15:44 +0200

    updated

--- mkgallery.py
+++ mkgallery.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
+"""
+Creates web gallery of images and fonts present in current directory
+"""
 import os, sys, re, getopt, shutil, glob
 
 # default values:
 title="default" # html title and gallery directory name
 resolution = 300 # thumbnail resolution
 url = "http://127.0.0.1:8080/galleries/" # browser url
+progress_len = 40 # progress bar length
 
 img_fmt  = re.compile('(jpg|png|gif|svg)$',re.I)
 font_fmt = re.compile('(otf|ttf)$',re.I)
@@ -12,12 +16,23 @@ fontname_re = re.compile('[^a-zA-Z0-9_]')
 
 def usage():#{{{
 		print """\
-usage: mkgallery.sh [gallery_name=default]
-        - creates new gallery (named "gallery_name") in script
-          directory; the gallery contains all images found in
-          current directory
-        - if BROWSER environment variable is set, the gallery
-          is viewed using web browser $BROWSER"""
+usage: %s [options] [gallery_name]
+
+    Creates new gallery (named "gallery_name") in script
+    directory; the gallery contains all images found in
+    current directory.
+
+    For program to be able to generate thumbnails, you
+    must have Python Imaging Library (PIL) is installed.
+
+    If BROWSER environment variable is set, the gallery
+    is automatically viewed using web browser $BROWSER
+
+options:
+    -h, --help              prints this help
+    -t, --title=<title>     gallery title and directory name (default: '%s')
+    -r, --resolution=<res>  resolution for thumbnails in pixels (default: %s)"""\
+			% (sys.argv[0], title, resolution)
 #}}}
 
 def walkdir(root):#{{{
@@ -113,19 +128,36 @@ def prepare_html(template,itemfile,css,imgdir):#{{{
 
 def create_thumbnails(imgdir,thumbdir,resolution):#{{{
 	try:
-		from PIL import Image
-
 		if os.path.isdir(thumbdir):
 			shutil.rmtree(thumbdir)
+
+		images = []
+		for f in walkdir(imgdir):
+			if img_fmt.search(f):
+				images.append(f)
+
+		n = len(images)
+		if n == 0: return
+
+		from PIL import Image
+
 		os.makedirs(thumbdir)
 
-		# TODO: show progress bar
-		for f in walkdir(imgdir):
-			if not img_fmt.search(f):
-				continue
+		i = 0
+		bar = ">" + (" "*progress_len)
+		sys.stdout.write( "Creating thumbnails: [%s] %d/%d\r"%(bar,0,n) );
+		for f in images:
 			im = Image.open(f)
 			im.thumbnail((resolution,resolution), Image.ANTIALIAS)
 			im.save(thumbdir+"/"+os.path.basename(f) + ".png", "PNG")
+
+			# TODO: show progress bar
+			i=i+1
+			l = i*progress_len/n
+			bar = ("="*l) + ">" + (" "*(progress_len-l))
+			sys.stdout.write( "Creating thumbnails: [%s] %d/%d%s"%(bar,i,n,i==n and "\n" or "\r") );
+			sys.stdout.flush()
+
 		print("Thumbnails successfully generated.")
 	except:
 		print("Warning: Generating thumbnails was unsuccessfull!")
@@ -159,7 +191,7 @@ def main(argv):#{{{
 	thumbdir = gdir+"/thumbs"
 	create_thumbnails(imgdir,thumbdir,resolution)
 
-	print("New gallery was created in "+gdir+" directory.")
+	print("New gallery was created in: '"+gdir+"'")
 #}}}
 
 if __name__ == "__main__":
